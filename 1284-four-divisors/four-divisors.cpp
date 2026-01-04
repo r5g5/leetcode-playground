6f16230fc45bcc56db9d917cf602154089952bff
Rahul Gupta <r5g5signin@gmail.com>
2026-01-04 09:54:15 +0530

    Time: 51 ms (34.73%) | Memory: 16.1 MB (92.61%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    inline const pair<int, int> getNoOfDivAndSum(const int num) {
+        int cnt = 0, sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                if (num / i == i) {
+                    cnt++;
+                    sum += i;
+                } else {
+                    cnt += 2;
+                    sum += i; 
+                    sum += (num / i);
+                }
+            }
+        }
+        return {cnt, sum};
+    }
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+        for (const int& num : nums) {
+            auto noSum = getNoOfDivAndSum(num);
+            if (noSum.first == 4) {
+                cout << "Num: " << num << endl;
+                ans += noSum.second;
+            }
+        }
+        return ans;
+    }
+};
