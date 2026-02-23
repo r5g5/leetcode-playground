8ff7dbd8c5bbdaa491ca8acca908539c46adb386
Rahul Gupta <r5g5signin@gmail.com>
2026-02-23 23:09:39 +0530

    Time: 548 ms (27.53%) | Memory: 150.5 MB (23.37%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -3,13 +3,9 @@ public:
     bool hasAllCodes(string s, int k) {
         unordered_set<string> codeSet;
         for (int i = 0, n = s.size() - k + 1; i < n; i++) {
-            string code = s.substr(i, k);
+            const string code = s.substr(i, k);
             codeSet.insert(code);
         }
-        // cout << codeSet.size() << endl;
-        // for (auto it = codeSet.begin(); it != codeSet.end(); it++) {
-        //     cout << *it << endl;
-        // }
         return codeSet.size() == 1 << k;
     }
 };
