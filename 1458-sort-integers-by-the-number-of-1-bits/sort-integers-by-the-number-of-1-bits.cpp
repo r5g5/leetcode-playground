dd65d3f6110d65b566d5db57752d4b12b8717f80
Rahul Gupta <r5g5signin@gmail.com>
2026-02-26 04:36:49 +0530

    Time: 4 ms (31.75%) | Memory: 14.1 MB (79.63%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,14 +1,25 @@
 class Solution {
+private:
+    int findWeight(int num) {
+        int weight = 0, mask = 1;
+        while (num > 0) {
+            if ((num & mask) > 0) {
+                weight++;
+                num ^= mask;
+            } 
+            mask <<= 1;
+        }
+        return weight;
+    }
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), [&] (const int numOne, const int numTwo) -> bool {
-            int noOfOnesInFirst = __builtin_popcount(numOne);
-            int noOfTwosInSecond = __builtin_popcount(numTwo);
-
-            if (noOfOnesInFirst == noOfTwosInSecond)
+        sort(arr.begin(), arr.end(), [this] (const int numOne, const int numTwo) -> bool {
+            int wOne = findWeight(numOne);
+            int wTwo = findWeight(numTwo);
+            if (wOne == wTwo) {
                 return numOne < numTwo;
-            
-            return noOfOnesInFirst < noOfTwosInSecond;
+            }
+            return wOne < wTwo;
         });
         return arr;
     }
