9556ac518d0107e6f23e876bafc99e724623d4c4
Rahul Gupta <r5g5signin@gmail.com>
2026-05-17 15:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (87.69%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         auto it = upper_bound(letters.begin(), letters.end(), target);
-        if (it == letters.end()) {
+        if (it == letters.end())
             return letters[0];
-        } else {
-            return *it;
-        }
+        return letters[it - letters.begin()];
     }
 };
