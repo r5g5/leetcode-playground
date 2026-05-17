07bb237fe4620774d29a10f67f570607653b4790
Rahul Gupta <r5g5signin@gmail.com>
2026-05-17 11:12:21 +0530

    Time: 0 ms (100.00%) | Memory: 34.8 MB (96.68%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
-        if (arr[start] == 0) return true;
-        const int n = arr.size();
-        queue<pair<int, int>> q;
-        vector<int> visited;
-        visited.resize(n, false);
-        q.push({start, arr[start]});
-        visited[start] = true;
-        // bool found = false;
-        while (!q.empty()) {
-            auto top = q.front(); q.pop();
-            int forward  = top.first + top.second;
-            int backward = top.first -  top.second;
-            if (forward >= 0 && forward < n && !visited[forward]) {
-                visited[forward] = true;
-                if (arr[forward] == 0)
-                    return true;
-                q.push({forward, arr[forward]});
-            }
-            if (backward >= 0 && backward < n && !visited[backward]) {
-                visited[backward] = true;
-                if (arr[backward] == 0)
-                    return true;
-                q.push({backward, arr[backward]});
+        if ((start >= 0) && (start < arr.size()) && (arr[start] >= 0)) {
+            if (arr[start] == 0)
+                return true;
+            else {
+                const int val = arr[start];
+                arr[start] = -1;
+                return canReach(arr, start - val) || canReach(arr, start + val);
             }
         }
         return false;
