fb493744142b873ce463f5f472928529fef0390a
Rahul Gupta <r5g5signin@gmail.com>
2026-05-22 22:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        auto it = find(nums.begin(), nums.end(), target);
-        if (it != nums.end())
-            return it - nums.begin();
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid -1;
+                }
+            }
+        }
+
         return -1;
     }
 };
