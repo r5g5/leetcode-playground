069864cf9b0ce05d4f602a4b2c61e9b667723659
Rahul Gupta <r5g5signin@gmail.com>
2026-06-18 21:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (46.25%) - LeetSync

---
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        // hour hand moves from 0 - 11 = 30H (360 / 12)
+        // also, in that one segment, it has 60 minutes = 30 / 60 = 0.5M
+        // minutes hand moves from 0 - 59 = 360 / 60 = 6M
+        // difference = 30H + 0.5M - 6M = 30H - 5.5M
+        double degree = abs(30 * hour - 5.5 * minutes);
+        return (degree > 180 ? 360 - degree : degree);
+    }
+};
