c01a1a8b6f0e3e02a83a2fc1d38bf8bc4e1b0961
Rahul Gupta <r5g5signin@gmail.com>
2026-06-22 22:41:14 +0530

    Time: 13 ms (14.83%) | Memory: 44.3 MB (14.36%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -0,0 +1,27 @@
+class Solution {
+    private static int getMin(int... nums) {
+        if (nums.length == 0) {
+            throw new IllegalArgumentException("Invalid arguement passed");
+        }
+        int ans = nums[0];
+        for (int num : nums) {
+            ans = Math.min(num, ans);
+        }
+        return ans;
+    }
+    public int maxNumberOfBalloons(String text) {
+        // final String balloon = "balloon"; // b -> 1, a -> 1, l -> 2, o -> 2, n -> 1
+        var charToCntMap = new HashMap<Character, Integer>();
+        for (char c : text.toCharArray()) {
+            if (!charToCntMap.containsKey(c)) {
+                charToCntMap.put(c, 0);
+            }
+            charToCntMap.put(c, charToCntMap.get(c) + 1);
+        }
+        return getMin(charToCntMap.getOrDefault('b', 0),
+                      charToCntMap.getOrDefault('a', 0),
+                      charToCntMap.getOrDefault('l', 0) / 2,
+                      charToCntMap.getOrDefault('o', 0) / 2,
+                      charToCntMap.getOrDefault('n', 0));
+    }
+}
