a90dd618e0ad47011f273faa499d98238d0e3c63
Rahul Gupta <r5g5signin@gmail.com>
2026-06-28 21:10:55 +0530

    Time: 6 ms (96.25%) | Memory: 77.3 MB (78.75%) - LeetSync

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        // First sort it, then the answer is min(size, arr[size])
+        final int n = arr.length;
+        Arrays.sort(arr);        
+        // First element must be 1, then all the next cons. elements must have a difference of at max 1.
+        int cnt = 1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= cnt) {
+                cnt++;
+            }
+        }
+        return cnt - 1;
+    }
+}
