177012cabed196af77c7e527a8719a84388dac69
Rahul Gupta <r5g5signin@gmail.com>
2026-07-15 22:45:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        /*
+        NOTE: sum of first n odd numbers = n^2
+        sum of first n even numbers = n^2 + n
+        From euclidean algorithms, gcd(a, b) = gcd(b, a % b); a > b
+        ====
+        gcd(n^2 + n, n^2) = gcd(n^2, n)
+        gcd(n^2, n) = gcd(n, 0) => n
+        */
+        return n; // TC: O(1), SC: O(1)
     }
 };
