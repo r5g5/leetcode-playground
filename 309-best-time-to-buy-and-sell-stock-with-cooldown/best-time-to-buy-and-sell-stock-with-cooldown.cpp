564919bebf298f5a1f885a1e2c67c21eaffa2029
Rahul Gupta <r5g5signin@gmail.com>
2026-07-20 00:41:12 +0530

    Time: 2 ms (39.06%) | Memory: 15.3 MB (74.95%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,27 +1,28 @@
 class Solution {
+private:
+    int dp[5001][2];
+    int dfs(const vector<int>& prices, int day, bool buying) {
+            if (day >= prices.size())
+                return 0;
+            if (dp[day][buying] != -1)
+                return dp[day][buying];
+            
+            int cooldown = dfs(prices, day + 1, buying);
+            
+            if (buying) {
+                int buy = dfs(prices, day + 1, !buying) - prices[day];
+                dp[day][buying] = max(cooldown, buy);
+            } else {
+                int sell = dfs(prices, day + 2, !buying) + prices[day];
+                dp[day][buying] = max(cooldown, sell);
+            }
+
+        return dp[day][buying];
+    }
+
 public:
     int maxProfit(vector<int>& prices) {
-        const int n = prices.size();
-        int dp[n][2];
         memset(dp, -1, sizeof(dp));
-        function<int(int, bool)> dfs = [&](int index, bool buying) -> int {
-            if (index >= prices.size()) {
-                return 0;
-            }
-            if (dp[index][buying] != -1) {
-                return dp[index][buying];
-            }
-            if (buying) {
-                int onBuying = dfs(index + 1, !buying) - prices[index];
-                int onCoolDown = dfs(index + 1, buying);
-                dp[index][buying] = max(onBuying, onCoolDown);
-            } else {
-                int onSelling = dfs(index + 2, !buying) + prices[index];
-                int onCoolDown = dfs(index + 1, buying);
-                dp[index][buying] = max(onSelling, onCoolDown);
-            }
-            return dp[index][buying];
-        };
-        return dfs(0, true);
+        return dfs(prices, 0, true); // TC: O(n), SC: O(1)
     }
 };
