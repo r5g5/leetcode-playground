9bec9e4f1711315352ffa288a99f4bc9f4d16578
Rahul Gupta <r5g5signin@gmail.com>
2026-07-28 23:13:32 +0530

    Time: 141 ms (17.80%) | Memory: 55.4 MB (83.74%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -2,30 +2,20 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         map<char, int> mp;
-        for (const char c : s) {
-            mp[c]++;
+        for (const auto& ch : s) {
+            mp[ch]++;
         }
-        const int n = s.size();
-        int i = 0, j = n - 1; 
+        int i = 0, j = s.size() - 1;
 
-        for (auto& kvp : mp) {
-            if (kvp.second & 1) {
-                int mid = kvp.second / 2;
-                for (int k = 0; k < mid; k++) {
-                    s[i++] = kvp.first;
-                }
-                for (int k = 0; k < mid; k++) {
-                    s[j--] = kvp.first;
-                }
-                s[n / 2] = kvp.first;
-            } else {
-                int mid = kvp.second / 2;
-                for (int k = 0; k < mid; k++) {
-                    s[i++] = kvp.first;
-                }
-                for (int k = 0; k < mid; k++) {
-                    s[j--] = kvp.first;
-                }
+        for (auto& [ch, cnt] : mp) {
+            int half = cnt / 2;
+            while (half--) {
+                s[i++] = ch;
+                s[j--] = ch;
+            }
+
+            if (cnt & 1) {
+                s[s.size() / 2] = ch;
             }
         }
 
