4b1615463b5ba41ca6d369eb3b29bcd21a9b607e
Rahul Gupta <r5g5signin@gmail.com>
2026-08-02 01:14:18 +0530

    Time: 0 ms (100.00%) | Memory: 60 MB (98.26%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-
-        vector<int> v = nums;
-
-        if (count(nums.begin(), nums.end(), 1) == nums.size()) {
-            return nums.size() - 1;
-        }
-
-        int cnt = 0;
+        int longestWindow = 0, start = 0, noOfZeroes = 0;
 
         for (int i = 0, n = nums.size(); i < n; i++) {
-            if (v[i] == 1) {
-                ++cnt;
-            } else {
-                nums[i] += cnt;
-                cnt = 0;
+            noOfZeroes += nums[i] == 0;
+
+            while (noOfZeroes > 1) {
+                noOfZeroes -= (nums[start] == 0);
+                start++;
             }
+
+            longestWindow = max(longestWindow, i - start);
         }
 
-        // for (int i : nums) {
-        //     cout << i << " ";
-        // }
-
-        cout << endl;
-
-        cnt = 0;
-        int ans = 0;
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            if (v[i] == 1) {
-                ++cnt;
-            } else {
-                nums[i] += cnt;
-                ans = max(ans, nums[i]);
-                cnt = 0;
-            }
-        }
-
-        // for (int i : nums) {
-        //     cout << i << " ";
-        // }
-
-
-        return ans; // TC: O(n), SC: O(n)
+        return longestWindow; // TC: O(n), SC: O(1)
     }
 };
