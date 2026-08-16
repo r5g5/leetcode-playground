97d609f83e676ce11e1e72e419d3cb8decf84b3c
Rahul Gupta <r5g5signin@gmail.com>
2026-08-16 16:10:37 +0530

    Time: 126 ms (6.69%) | Memory: 56 MB (5.64%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
+        unordered_map<string, int> rowCounter;
         int count = 0;
-        for (int row = 0, n = grid.size(); row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                bool isMatch = true;
-                for (int i = 0; i < n; i++) {
-                    if (grid[row][i] != grid[i][col]) {
-                        isMatch = false;
-                        break;
-                    }
-                }
-                count += isMatch ? 1 : 0;
+        for (auto& row : grid) {
+            string s;
+            for_each(row.begin(), row.end(), [&](const int x) -> void {s.append(std::to_string(x) + ",");});
+            rowCounter[s]++;
+        }
+
+        for (int r = 0, n = grid.size(); r < n; r++) {
+            vector<int> column;
+            for (int c = 0; c < n; c++) {
+                column.push_back(grid[c][r]);
+            }
+            string s;
+            for_each(column.begin(), column.end(), [&](const int x) -> void {s.append(std::to_string(x) + ",");});
+            if (rowCounter.find(s) != rowCounter.end()) {
+                count += rowCounter[s];
             }
         }
-        return count; // TC: O(n^3), SC: O(1)
+        return count; // TC: O(n^2), SC: O(n)
     }
 };
