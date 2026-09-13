b5467754ddeea42eda855f77e7ca1db6f5120da3
Rahul Gupta <r5g5signin@gmail.com>
2026-09-13 17:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (54.11%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    int dp[1001];
+    int solve(const int curr, const int top, const vector<int>& cost) {
+        if (curr >= top) {
+            return 0;
+        } else if (dp[curr] != -1) {
+            return dp[curr];
+        } else {
+            return dp[curr] = min(
+                cost[curr] + solve(curr + 1, top, cost),
+                cost[curr] + solve(curr + 2, top, cost)
+            );
+        }
+    }
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        memset(dp, -1, sizeof(dp));
+        return min(solve(0, cost.size(), cost), solve(1, cost.size(), cost)); // TC: O(2^n), SC: O(n)
+    }
+};
