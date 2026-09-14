17d8a03c888dae6f48b739d3a1c74074843979ae
Rahul Gupta <r5g5signin@gmail.com>
2026-09-14 20:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (95.69%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void getLeafNodes(vector<int>& leafs, TreeNode* head) {
+        if (!head) return;
+        if (head->left == nullptr && head->right == nullptr) {
+            leafs.emplace_back(head->val);
+            return;
+        }
+        getLeafNodes(leafs, head->left);
+        getLeafNodes(leafs, head->right);
+    }
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leafs1, leafs2;
+        getLeafNodes(leafs1, root1);
+        getLeafNodes(leafs2, root2);
+
+        if (leafs1.size() != leafs2.size()) return false;
+
+        for (int i = 0, n = leafs1.size(); i < n; i++) {
+            // cout << leafs1[i] << "|| " << leafs2[i] << endl;
+            if (leafs1[i] != leafs2[i])
+                return false;
+        }
+        return true; // TC: O(n+m), SC: O(n)
+    }
+};
